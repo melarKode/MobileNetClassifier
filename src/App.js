0512b48d170a2835e2baca02f3f42fbdfcea2882
Navneet Melarkode <navneet.melarkode@gmail.com>
2020-09-01 13:31:17 +0530

    Mobilenet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React,{useReducer} from 'react';
+import * as tf from '@tensorflow/tfjs';
+import React,{useReducer, useState} from 'react';
+import * as mobilenet from '@tensorflow-models/mobilenet';
 import './App.css';
 
 const stateMachine = {
@@ -7,19 +9,39 @@ const stateMachine = {
     initial: {on: {next:'loadingModel'}},
     loadingModel: {on: {next:'awaitingUpload'}},
     awaitingUpload: {on: {next:'ready'}},
-    ready: {on: {next:'classifying'}},
+    ready: {on: {next:'classifying'}, showImage: true},
     classifying: {on:{next:'complete'}},
-    complete: {on: {next:'awaitingUpload'}}
+    complete: {on: {next:'awaitingUpload'}, showImage: true}
   } 
 }
 
 const reducer = (currentState,event)=> stateMachine.states[currentState].on[event] || stateMachine.initial;
 
 function App() {
+  tf.setBackend("cpu");
   const [state, dispatch] = useReducer(reducer, stateMachine.initial);
+  const [model, setModel] = useState(null);
+  const next = ()=> dispatch('next');
+
+  const loadModel = async ()=>{
+    console.log('hi')
+    next();
+    const mobilenetModel = await mobilenet.load();
+    setModel(mobilenetModel);
+    next();
+  }
+
+  const buttonProps = {
+    initial: {text: 'Load Model',action:loadModel},
+    loadingModel: {text: 'Loading Model...', action: ()=>{}},
+    awaitingUpload: {text: 'Upload Photo',action: ()=>{}},
+    ready: {text: 'Identify', action: ()=>{}},
+    classifying: {text: 'Identifying', action: ()=>{}},
+    complete: {text: 'Reset', action: ()=>{}},
+  }
   return (
     <div className="App">
-      <button onClick={()=>dispatch('next')}>{state}</button>
+      <button onClick={buttonProps[state].action}>{buttonProps[state].text}</button>
     </div>
   );
 }
